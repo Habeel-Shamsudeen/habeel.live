6d4372228ba31f1785f180e9049fd9ba56567ee9
Habeel <habeelshamsudeen9895@gmail.com>
2025-08-04 16:59:42 +0530

    OG-initial copy

---
+++ src/data/user.ts
@@ -0,0 +1,53 @@
+export const USER = {
+  firstName: "Habeel",
+  lastName: "Shamsudeen",
+  displayName: "Habeel",
+  username: "habeel",
+  gender: "male",
+  pronouns: "he/him",
+  bio: "Creating with code. Small details matter.",
+  flipSentences: [
+    "Creating with code. Small details matter.",
+    "Design Engineer",
+    "Open Source Contributor",
+  ],
+  address: "Kerala, India",
+  phoneNumber: "KzkxODA3NTgxNDc0Ng", // E.164 format, base64 encoded (https://t.io.vn/base64-string-converter)
+  email: "aGFiZWVsODA3NUBnbWFpbC5jb20=", // base64 encoded
+  website: "https://habeel.live",
+  otherWebsites: [
+    "https://gradeit.habeel.live",
+    "https://vserv.habeel.live",
+    "https://taskflow.habeel.live",
+  ],
+  jobTitle: "Software Developer",
+  jobs: [
+    {
+      title: "Software Developer",
+      company: "OddsView",
+      website: "https://oddsview.com",
+    },
+    {
+      title: "Founder",
+      company: "GradeIt",
+      website: "https://gradeit.habeel.live",
+    },
+  ],
+  about: `
+Hello, World! I am Habeel — a Software Engineer passionate about creating high-performance, user-centric software solutions with intuitive and engaging designs.
+
+With 1+ years of experience, I specialize in building high-quality web and mobile applications using Next.js, React, TypeScript, and modern front-end technologies. Beyond work, I love exploring new technologies and turning ideas into reality through personal projects.
+
+One of my key projects, [ZaDark](https://zadark.com), enhances the Zalo experience on PC and Web, surpassing 80,000 downloads on [SourceForge](https://sourceforge.net/projects/zadark) and 15,000 active users on the [Chrome Web Store](https://chromewebstore.google.com/detail/llfhpkkeljlgnjgkholeppfnepmjppob) since 2022.
+
+I'm also the creator of [React Wheel Picker](https://react-wheel-picker.chanhdai.com) — iOS-like wheel picker for React with smooth inertia scrolling and infinite loop support. It has earned 221 stars on [GitHub](https://github.com/ncdai/react-wheel-picker) and was selected for **Vercel's Open Source Program** summer 2025 cohort, receiving $3,600 in Vercel platform credits, an OSS Starter Pack, and priority community support to fuel its ongoing development.
+
+Let's connect and collaborate!
+  `,
+  avatar: "https://assets.chanhdai.com/images/chanhdai-avatar-ghibli.jpeg",
+  ogImage: "https://assets.chanhdai.com/images/screenshot-og-image-dark.png",
+  namePronunciationUrl: "https://assets.chanhdai.com/audio/chanhdai.mp3",
+  keywords:
+    "ncdai, nguyenchanhdai, nguyen chanh dai, chanhdai, chanh dai, iamncdai, quaric, zadark, nguyễn chánh đại, chánh đại",
+  dateCreated: "2023-10-20", // YYYY-MM-DD
+};
