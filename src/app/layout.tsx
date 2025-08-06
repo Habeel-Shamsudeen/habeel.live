5c24b5dc29011780c2105213ab6fdf56c680c531
Habeel <habeelshamsudeen9895@gmail.com>
2025-08-06 14:02:27 +0530

    Updated icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,16 +77,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "https://assets.chanhdai.com/images/favicon.ico",
+        url: "https://raw.githubusercontent.com/Habeel-Shamsudeen/habeel.live/main/public/favicon/favicon.ico",
         sizes: "any",
       },
       {
-        url: "https://assets.chanhdai.com/images/favicon.svg",
+        url: "https://raw.githubusercontent.com/Habeel-Shamsudeen/habeel.live/main/public/favicon/favicon.svg",
         type: "image/svg+xml",
       },
     ],
     apple: {
-      url: "https://assets.chanhdai.com/images/apple-touch-icon.png",
+      url: "https://raw.githubusercontent.com/Habeel-Shamsudeen/habeel.live/main/public/icons/apple-touch-icon.png",
       type: "image/png",
       sizes: "180x180",
     },
